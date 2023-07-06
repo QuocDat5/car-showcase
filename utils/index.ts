f113590ca685e526772cbb60d7b4c3b83f1db3d1
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-06 22:24:37 +0700

    rapid api request preparation

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a0511a10bcmsh74c3933e1ddd12ep1cdd13jsne299a6c24e26',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
