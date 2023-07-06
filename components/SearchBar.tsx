5d047685402e83059dc25e300f72c54936f4536a
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-06 20:19:44 +0700

    installed headless ui

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
+import { SearchManufacturer } from "./";
+
 const SearchBar = () => {
-    return <div>SearchBar</div>;
+    const [manufacturer, setManufacturer] = useState("");
+
+    const handleSearch = () => {};
+
+    return (
+        <form className="searchbar" onClick={handleSearch}>
+            <div className="searchbar__item">
+                <SearchManufacturer
+                    manufacturer={manufacturer}
+                    setManufacturer={setManufacturer}
+                />
+            </div>
+        </form>
+    );
 };
 
 export default SearchBar;
