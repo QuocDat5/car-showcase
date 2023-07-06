ca0f858a4b0584ccaf89ca5281b8b20eb03a4016
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-06 22:32:32 +0700

    added a function to fetch data from rapid api

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,15 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a0511a10bcmsh74c3933e1ddd12ep1cdd13jsne299a6c24e26',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
+export async function fetchCars() {
+    const headers = {
+        "X-RapidAPI-Key": "a0511a10bcmsh74c3933e1ddd12ep1cdd13jsne299a6c24e26",
+        "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+    const response = await fetch(
+        "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+        { headers: headers }
+    );
+
+    const result = await response.json();
+
+    return result;
 }
