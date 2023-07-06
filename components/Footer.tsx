6434799220a54dd7fba6187552232efe5cbc5156
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-06 17:12:05 +0700

    added logo & links to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,52 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { footerLinks } from "@/constants";
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+            <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+                <div className="flex flex-col justify-start items-start gap-6">
+                    {/* logo */}
+                    <Image
+                        src="/logo.svg"
+                        alt="Car Hub logo"
+                        width={118}
+                        height={18}
+                        className="object-contain"
+                    />
+
+                    {/* copyright text */}
+                    <p className="text-base text-gray-700">
+                        Car Hub 2023 <br />
+                        All rights reserved &copy;
+                    </p>
+                </div>
+
+                {/* footer links */}
+                <div className="footer__links">
+                    {footerLinks.map((link) => (
+                        <div className="footer__link" key={link.title}>
+                            {/* titles */}
+                            <h3 className="font-bold">{link.title}</h3>
+
+                            {/* links */}
+                            {link.links.map((item) => (
+                                <Link
+                                    key={item.title}
+                                    href={item.url}
+                                    className="text-gray-500"
+                                >
+                                    {item.title}
+                                </Link>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,6 +7,7 @@ const NavBar = () => {
     return (
         <header className="w-full absolute z-10">
             <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+                {/* logo */}
                 <Link href="/" className="flex justify-center items-center">
                     <Image
                         src="/logo.svg"
@@ -17,6 +18,7 @@ const NavBar = () => {
                     />
                 </Link>
 
+                {/* signin button */}
                 <CustomButton
                     title="Sign in"
                     btnType="button"
