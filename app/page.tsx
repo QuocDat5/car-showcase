81e4380535124a0a7de718ab75a94f3d41801f4c
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-06 17:34:17 +0700

    created search bar & filter components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { Hero } from "@/components";
+import { Hero, SearchBar, CustomFilter } from "@/components";
 
 export default function Home() {
     return (
