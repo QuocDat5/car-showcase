6c3c159090b1edb09b8cefc721503b93e5932be9
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-06 21:14:47 +0700

    search manufacturer: complete

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -25,7 +25,7 @@ const SearchManufacturer = ({
 
     return (
         <div className="search-manufacturer">
-            <Combobox>
+            <Combobox value={manufacturer} onChange={setManufacturer}>
                 <div className="relative w-full">
                     <Combobox.Button className="absolute top-[14px]">
                         <Image
@@ -52,21 +52,50 @@ const SearchManufacturer = ({
                         afterLeave={() => setQuery("")}
                     >
                         <Combobox.Options>
-                            {filteredManufacturers.map((item) => (
-                                <Combobox.Option
-                                    key={item}
-                                    className={({ active }) =>
-                                        `relative search-manufacturer__option ${
-                                            active
-                                                ? "bg-primary-blue text-white"
-                                                : "text-gray-900"
-                                        }`
-                                    }
-                                    value={item}
-                                >
-                                    {item}
-                                </Combobox.Option>
-                            ))}
+                            {filteredManufacturers.length === 0 &&
+                            query !== "" ? (
+                                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                                    Nothing found.
+                                </div>
+                            ) : (
+                                filteredManufacturers.map((item) => (
+                                    <Combobox.Option
+                                        key={item}
+                                        className={({ active }) =>
+                                            `relative search-manufacturer__option ${
+                                                active
+                                                    ? "bg-primary-blue text-white"
+                                                    : "text-gray-900"
+                                            }`
+                                        }
+                                        value={item}
+                                    >
+                                        {({ selected, active }) => (
+                                            <>
+                                                <span
+                                                    className={`block truncate ${
+                                                        selected
+                                                            ? "font-medium"
+                                                            : "font-normal"
+                                                    }`}
+                                                >
+                                                    {item}
+                                                </span>
+
+                                                {selected ? (
+                                                    <span
+                                                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                                            active
+                                                                ? "text-white"
+                                                                : "text-teal-600"
+                                                        }`}
+                                                    ></span>
+                                                ) : null}
+                                            </>
+                                        )}
+                                    </Combobox.Option>
+                                ))
+                            )}
                         </Combobox.Options>
                     </Transition>
                 </div>
