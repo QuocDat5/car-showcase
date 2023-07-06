65db74b7f1735a93f88f1cc7df63f9352ab4d44f
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-06 22:58:34 +0700

    added car card component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,5 +1,31 @@
-const CarCard = () => {
-    return <div>CarCard</div>;
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import { CarProps } from "@/types";
+import CustomButton from "./CustomButton";
+
+interface CarCardProps {
+    car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+    const { city_mpg, year, make, model, transmission, drive } = car;
+
+    return (
+        <div className="car-card group">
+            <div className="car-card__content">
+                <h2 className="car-card__content-title">
+                    {make} {model}
+                </h2>
+            </div>
+
+            <p>
+                <span>Car rent.</span>
+            </p>
+        </div>
+    );
 };
 
 export default CarCard;
